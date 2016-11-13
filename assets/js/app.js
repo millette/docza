e24df24485b3d11e10f0492f2911e1e474c90e28
Robin Millette <robin@millette.info>
2016-11-12 19:08:15 -0500

    Use medium-editor in client-side js.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,13 @@
-/* global $ */
+/* global $, MediumEditor */
 
 $(() => {
+  const editor = $('.wysiwyg')[0]
   $(document).foundation()
+  if (editor) {
+    new MediumEditor(editor, { // eslint-disable-line no-new
+      placeholder: {
+        text: 'Entrez votre texte. Sélectionnez en une partie pour voir le toolbar de styles.'
+      }
+    })
+  }
 })
-
