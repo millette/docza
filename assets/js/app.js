60307c6e675dd845b72ebc93aefae5d28b1f1bb1
Robin Millette <robin@millette.info>
2016-11-14 19:26:51 -0500

    Completely remove medium-editor.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,5 @@
-/* global $, MediumEditor */
+/* global $ */
 
 $(() => {
   $(document).foundation()
-  const editor = $('.wysiwyg')[0]
-  if (editor) {
-    new MediumEditor(editor, { // eslint-disable-line no-new
-      placeholder: {
-        text: 'Entrez votre texte. Sélectionnez en une partie pour voir le toolbar de styles.'
-      }
-    })
-  }
 })
